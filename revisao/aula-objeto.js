115faaf493ef1fb2b79b463438b0b8082cbc8eaf
Bryan Machado <bryan.machado.soares@gmail.com>
2024-02-09 17:56:00 -0300

    add folder revisao, object study file

---
+++ revisao/aula-objeto.js
@@ -0,0 +1,29 @@
+const endereco = {
+    rua: 'Disneylandia',
+    numero: 1
+}
+
+const user = {
+    nome: 'Pirulebes',
+    idade: 39,
+    email: 'pirulebes@gmail.com',
+    prof: true,
+    end: endereco,
+    familia: {
+        mae: 'João',
+        pai: 'Maria',
+        irmão: 'Pirulebes'
+    },
+    pets: [{nome: 'tobias', animal: 'cachorro'}, 'gato', 'peixe']
+}
+
+delete user.prof
+console.log(user);
+user.pets[0] = {sla: 'nome varial', teste: 'teste dois'}
+console.log(user);
+console.log(user.pets[0]);
+
+user.idade = 100000
+console.log(user.idade)
+console.log(user.familia.mae);
+console.log(user.prof);

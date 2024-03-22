48eb7738be2bf57f03bd20a8f6cd17bb6eaf93d9
Bryan-Machado <bryan.machado.soares@gmail.com>
2024-03-22 14:56:33 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 import express from 'express'
 const app = express()
 import {PORT, HOST} from './config.js'
+import cors from 'cors'
 
 
 import logger  from './middlewares/logger.js'
@@ -10,6 +11,11 @@ import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
 
 app.use(express.json())
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:8081', 'http://meusite.com'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(logger)
 
 app.use('/user', userRouter)
